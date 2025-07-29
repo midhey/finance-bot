a6488fe38ec1a5aae38fa62bc982f4e212992ef3
Alexey Semenov <midhey@yandex.ru>
2025-07-30 00:25:45 +0300

    feat(bot): add bot entrypoint

--- app/bot.py
+++ app/bot.py
@@ -1,1 +1,23 @@
-print("test")
+from aiogram import Bot, Dispatcher
+from aiogram.types import Update
+from aiogram.fsm.storage.redis import RedisStorage
+
+from app.general.state import state
+from app.config import settings
+from app.middlewares.db_session import DBSessionMiddleware
+
+from app.handlers.group import group_router
+
+
+bot = Bot(token=settings.BOT_TOKEN)
+dp = Dispatcher(storage=RedisStorage(state.redis))
+
+dp.message.middleware(DBSessionMiddleware(state.db))
+dp.callback_query.middleware(DBSessionMiddleware(state.db))
+
+dp.include_router(group_router)
+
+
+async def handle_update(data: dict):
+    update = Update.model_validate(data)
+    await dp.feed_update(bot, update)
